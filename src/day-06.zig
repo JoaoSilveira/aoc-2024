480d81db61974abcfcd92c5f340357ba6575b4b2
João Pedro <joao.pedro0842@hotmail.com>
2024-12-07 08:13:40 -0300

    Solve day 6 with brute force

--- src/day-06.zig
+++ src/day-06.zig
@@ -105,78 +105,48 @@ const Position = struct {
     }
 };
 
-const LoopDetector = struct {
-    last_four_pos: [4]Position,
-    length: usize, // how many positions are there
-
-    pub fn init() @This() {
-        return @This(){
-            .last_four_pos = std.mem.zeroes([4]Position),
-            .length = 0,
-        };
-    }
-
-    pub fn appendPos(self: *@This(), pos: Position) ?Position {
-        if (self.length < 4) {
-            self.last_four_pos[self.length] = pos;
-            self.length += 1;
-            return null;
-        }
-        defer {
-            std.mem.copyForwards(
-                Position,
-                self.last_four_pos[0..3],
-                self.last_four_pos[1..4],
-            );
-            self.last_four_pos[3] = pos;
-        }
-
-        const p1 = self.last_four_pos[0];
-        const p2 = self.last_four_pos[1];
-        const p4 = self.last_four_pos[3];
-        const p5 = pos;
-
-        const has_loop = linesIntersect(p1, p2, p4, p5);
-
-        std.debug.print("<path d=\"M{d} {d}L{d} {d}M{d} {d}L{d} {d}\" stroke-width=\".1\" stroke=\"black\"/><!--{any}-->\n", .{
-            p1.x,     p1.y,
-            p2.x,     p2.y,
-            p4.x,     p4.y,
-            p5.x,     p5.y,
-            has_loop,
-        });
-        if (!has_loop) return null;
-
-        if (p1.x == p2.x) {
-            return Position{
-                .x = p1.x,
-                .y = p5.y,
-            };
-        }
-
-        return Position{
-            .x = p5.x,
-            .y = p1.y,
-        };
-    }
-
-    fn linesIntersect(p1: Position, p2: Position, p4: Position, p5: Position) bool {
-        if (p1.x > p2.x)
-            return linesIntersect(p2, p1, p4, p5);
-
-        if (p4.x > p5.x)
-            return linesIntersect(p1, p2, p5, p4);
-
-        if (p1.y > p2.y)
-            return linesIntersect(p2, p1, p4, p5);
-
-        if (p4.y > p5.y)
-            return linesIntersect(p1, p2, p5, p4);
-
-        return (p1.x <= p4.x and p2.x >= p4.x and p4.y <= p1.y and p5.y >= p1.y) or
-            (p4.x <= p1.x and p5.x >= p1.x and p1.y <= p4.y and p2.y >= p4.y);
-    }
+const SimmulationResult = enum {
+    moves_outside,
+    loops,
 };
+
+fn simmulateGuard(matrix: Matrix, init_pos: Position, init_dir: Direction) !SimmulationResult {
+    var hit_obstacles = std.ArrayList(struct { Position, Direction }).init(std.heap.page_allocator);
+    defer hit_obstacles.deinit();
+
+    var pos = init_pos;
+    var dir = init_dir;
+
+    while (true) {
+        const next_pos = pos.moveTo(dir);
+
+        if (next_pos == null or matrix.charAt(next_pos.?) == null) {
+            return .moves_outside;
+        }
+
+        // obstacle or wall
+        if (next_pos == null or (matrix.charAt(next_pos.?) orelse '#') == '#') {
+            const hit = .{ pos, dir };
+
+            for (hit_obstacles.items) |obstacle| {
+                if (std.mem.eql(u8, std.mem.asBytes(&hit), std.mem.asBytes(&obstacle))) {
+                    return .loops;
+                }
+            }
+
+            try hit_obstacles.append(hit);
+            dir = dir.rotateRight();
+
+            continue;
+        }
+
+        if (next_pos) |next| {
+            pos = next;
+        } else {
+            @panic("should never happen because of the previous if");
+        }
+    }
+}
 
 pub fn execute() !void {
     var file = try std.fs.cwd().openFile("data/day-06.txt", .{});
@@ -191,49 +161,26 @@ pub fn execute() !void {
         std.math.maxInt(usize),
     );
 
-    // var hit_obstacles = std.ArrayList(struct { Position, Direction }).init(std.heap.page_allocator);
-    // defer hit_obstacles.deinit();
-
     var matrix = try Matrix.init(content.items);
-    var direction = Direction.north;
-    var loop = LoopDetector.init();
-    var pos = matrix.offsetToPos(std.mem.indexOfScalar(u8, content.items, '^') orelse return error.MissingGuard) orelse return error.InvalidStartPosition;
+    const direction = Direction.north;
+    const pos = matrix.offsetToPos(std.mem.indexOfScalar(u8, content.items, '^') orelse return error.MissingGuard) orelse return error.InvalidStartPosition;
 
-    _ = loop.appendPos(pos);
     var total: usize = 0;
-    // walk_loop:
-    while (true) {
-        const next_pos = pos.moveTo(direction);
 
-        if (next_pos == null or matrix.charAt(next_pos.?) == null) {
-            total += @intFromBool(loop.appendPos(pos) != null);
-            break;
-        }
+    for (0..matrix.lines()) |line| {
+        for (0..matrix.columns()) |column| {
+            const obstacle_pos = Position{ .x = column, .y = line };
 
-        // obstacle or wall
-        if (next_pos == null or (matrix.charAt(next_pos.?) orelse '#') == '#') {
-            total += @intFromBool(loop.appendPos(pos) != null);
+            if (matrix.charAt(obstacle_pos) == '#')
+                continue;
 
-            // const hit = .{ pos, direction };
+            matrix.setCharAt('#', obstacle_pos);
+            defer matrix.setCharAt('.', obstacle_pos);
 
-            // for (hit_obstacles.items) |obstacle| {
-            //     if (std.mem.eql(u8, std.mem.asBytes(&hit), std.mem.asBytes(&obstacle))) {
-            //         break :walk_loop;
-            //     }
-            // }
-
-            // try hit_obstacles.append(hit);
-            direction = direction.rotateRight();
-
-            continue;
-        }
-
-        if (next_pos) |next| {
-            // matrix.setCharAt('^', next);
-
-            pos = next;
-        } else {
-            @panic("should never happen because of the previous if");
+            switch (try simmulateGuard(matrix, pos, direction)) {
+                .loops => total += 1,
+                .moves_outside => {},
+            }
         }
     }
 
